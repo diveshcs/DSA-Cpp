b1bed60faa07175fa9cb9ce7882f2017304bb87d
diveshcs <divesh190106@gmail.com>
2026-05-11 20:22:37 +0530

    Added group anagrams problem

---
+++ Strings/M_group_anagrams.cpp
@@ -0,0 +1,23 @@
+// LeetCode 49: Group Anagrams
+// Topic: String, Hashmap approach
+// Difficulty: Medium
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string,vector<string>> mp;
+        for(int i = 0; i < strs.size(); i++){
+            string sorted = strs[i];
+            sort(sorted.begin(),sorted.end());
+            mp[sorted].push_back(strs[i]);
+        }
+        for(auto it : mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
